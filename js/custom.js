46a5a0931595410d56c8141507dbc81748d00a7f
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 23:46:48 -0600

    js fix

--- js/custom.js
+++ js/custom.js
@@ -19,9 +19,9 @@ $(function() {
 function loadDiv(page) {
 	$("#divDisplay").html('<br/><div class="progress progress-striped active"><div class="progress-bar progress-bar-success"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div></div>');
 	//$("#divDisplay").load(page+".php");
-	$(".progress-bar").animate({width: "100%"},1000);
+	$(".progress-bar").animate({width: "100%"},800);
 
-	$("#divDisplay").delay(1500).queue(function(n){
+	$("#divDisplay").delay(1000).queue(function(n){
 		$(this).load(page+".html", function(response, status, xhr){
 			if (status == "error") {
 				loadDiv('home');
