240594e16408ba28b7bf1270f82e761d7556a905
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 22:48:55 -0600

    alert fix

--- js/custom.js
+++ js/custom.js
@@ -18,7 +18,7 @@ $(function() {
 });
 
 function loadDiv(page) {
-	$(".alert").hide();
+	$("#alertLogin").hide();
 	$("#divDisplay").html('<br/><div class="progress progress-striped active"><div class="progress-bar progress-bar-success"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div></div>');
 	//$("#divDisplay").load(page+".php");
 	$(".progress-bar").animate({width: "100%"},1000);
