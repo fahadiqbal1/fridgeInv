111e0d764124f82aa82a042ab62b759072bae363
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 19:08:41 -0600

    form fixes

--- login.php
+++ login.php
@@ -9,32 +9,32 @@
 					<div class="control-group">
 						<label class="control-label" for="inputCompanyName">Company Name</label>
 						<div class="controls">
-							<input type="text" id="inputCompanyName" placeholder="E.g. Some Software Pvt. Ltd.">
+							<input type="text" class="form-control" id="inputCompanyName" placeholder="E.g. Some Software Pvt. Ltd.">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputUsername">Username</label>
 						<div class="controls">
-							<input type="text" id="inputUsername" placeholder="E.g. ashwinhegde">
+							<input type="text" class="form-control" id="inputUsername" placeholder="E.g. ashwinhegde">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputPassword">Password</label>
 						<div class="controls">
-							<input type="password" id="inputPassword" placeholder="Min. 8 Characters">
+							<input type="password" class="form-control" id="inputPassword" placeholder="Min. 8 Characters">
 						</div>
 					</div>
 					<div class="control-group">
 						<div class="controls">
 							<label class="checkbox">
-								<input type="checkbox"> Keep me signed in ¦
+								<input type="checkbox"> Keep me signed in
 								<a href="#" class="btn btn-link">Forgot my password</a>
 							</label>
 							<button type="submit" class="btn btn-success">Sign In</button>
-							<button type="button" class="btn">Help</button>
+							<button type="button" class="btn btn-default">Help</button>
 						</div>
 					</div>
-					<div class="alert alert-error">
+					<div class="alert alert-danger">
 						<button type="button" class="close" data-dismiss="alert">×</button>
 						<strong>Access Denied!</strong> Please provide valid authorization.
 					</div>
@@ -50,37 +50,37 @@
 					<div class="control-group">
 						<label class="control-label" for="inputCompanyName">Company Name</label>
 						<div class="controls">
-							<input type="text" id="inputCompanyName" placeholder="E.g. Some Software Pvt. Ltd.">
+							<input type="text" class="form-control" id="inputCompanyName" placeholder="E.g. Some Software Pvt. Ltd.">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputFirst">First Name</label>
 						<div class="controls">
-							<input type="text" id="inputFirst" placeholder="E.g. Ashwin">
+							<input type="text" class="form-control" id="inputFirst" placeholder="E.g. Ashwin">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputLast">Last Name</label>
 						<div class="controls">
-							<input type="text" id="inputLast" placeholder="E.g. Hegde">
+							<input type="text" class="form-control" id="inputLast" placeholder="E.g. Hegde">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputEmail">Email</label>
 						<div class="controls">
-							<input type="text" id="inputEmail" placeholder="E.g. ashwinh@cybage.com">
+							<input type="text" class="form-control" id="inputEmail" placeholder="E.g. ashwinh@cybage.com">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputUser">Username</label>
 						<div class="controls">
-							<input type="text" id="inputUser" placeholder="E.g. ashwinhegde">
+							<input type="text" class="form-control" id="inputUser" placeholder="E.g. ashwinhegde">
 						</div>
 					</div>
 					<div class="control-group">
 						<label class="control-label" for="inputPassword">Password</label>
 						<div class="controls">
-							<input type="password" id="inputPassword" placeholder="Min. 8 Characters">
+							<input type="password" class="form-control" id="inputPassword" placeholder="Min. 8 Characters">
 						</div>
 					</div>
 					<div class="control-group">
@@ -89,7 +89,7 @@
 								<input type="checkbox"> I agree all your <a href="#">Terms of Services</a>
 							</label>
 							<button type="submit" class="btn btn-success">Sign Up</button>
-							<button type="button" class="btn">Help</button>
+							<button type="button" class="btn btn-default">Help</button>
 						</div>
 					</div>
 					<div class="alert alert-info">
