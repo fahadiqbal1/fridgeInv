6aaf7d700dd4a17308e9a9825f930c583285d935
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 18:13:51 -0600

    Added bootstrap

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<?php ?>
+<!DOCTYPE html>
 <html>
-<head>
-</head>
-<body>
-<h1>Hello</h1>
-</body>
+	<head>
+		<title>Fridge Inventory</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+
+		<script src="//code.jquery.com/jquery.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+		<script src="js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<!--TODO Add HTML Code -->
+		<h1>Hello, world!</h1>
+	</body>
 </html>
