eea9b07ddc34a391c7bfda383b46a9989d3088ce
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-08 17:47:14 -0600

    signup insert

--- action/formSignUp.php
+++ action/formSignUp.php
@@ -3,23 +3,30 @@
 	$output = '';
 
 	if(isset($_POST['inputFirst'])){
-		$fName = $_POST['inputFirst'];
+		$fName = sanitise($_POST['inputFirst']);
 	}
 	if(isset($_POST['inputLast'])){
-		$lName = $_POST['inputLast'];
+		$lName = sanitise($_POST['inputLast']);
 	}
 	if(isset($_POST['inputEmail'])){
-		$email = $_POST['inputEmail'];
+		$email = sanitise($_POST['inputEmail']);
 	}
 	if(isset($_POST['inputPass'])){
-		$pass = $_POST['inputPass'];
+		$pass = sanitise($_POST['inputPass']);
 	}
 	if(isset($_POST['inputUser'])){
-		$user = $_POST['inputUser'];
+		$user = sanitise($_POST['inputUser']);
 	}
 
 	function lenCheck($checkVar){
 		return strlen($checkVar);
+	}
+	function sanitise($string){
+		$string = strip_tags($string); // Remove HTML
+		$string = htmlspecialchars($string); // Convert characters
+		$string = trim(rtrim(ltrim($string))); // Remove spaces
+		$string = mysql_real_escape_string($string); // Prevent SQL Injection
+		return $string;
 	}
 
 	if(lenCheck($pass) < 8){
@@ -27,6 +34,12 @@
 	}
 	elseif(lenCheck($user) < 2){
 		$output = "Username needs to be more than 2 characters";
+	}
+	else{
+		$pass = md5($pass);
+		$sql = "INSERT INTO `db_fi_learn`.`fridInv.tblUsers` (`userId` ,`userName` ,`password` ,`firstName` ,`lastName` ,`email` ,`createdDtTm`)
+				VALUES (NULL , '".$user."', '".$pass."', '".$fName."', '".$lName."', '".$email."',CURRENT_TIMESTAMP);";
+		mysqli_query($con,$sql) or die ($output = mysqli_error($con));
 	}
 
 
