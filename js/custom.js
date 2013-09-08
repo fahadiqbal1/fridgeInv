1f7bd031a3591d9df913f5f3db92b9c5b759b9c9
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 20:21:36 -0600

    js test

--- js/custom.js
+++ js/custom.js
@@ -7,7 +7,7 @@ $(function() {
 function loadDiv(page) {
 	$("#divDisplay").html('<div class="progress progress-striped active"><div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 55%"></div></div>');
 	//$("#divDisplay").load(page+".php");
-
+	$(".progress-bar").animate({width: "100%"},1000);
 
 	$("#divDisplay").delay(1000).queue(function(n){
 		$(this).load(page+".php");
