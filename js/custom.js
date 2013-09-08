f51dc5bcfe72f3b8d293b761c560b4f0e474fc31
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 20:02:36 -0600

    js fix

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,6 @@ $(function() {
 });
 
 function loadDiv(page) {
-	$("#divDisplay").html('<div class="progress progress-striped active"><br/><div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%"></div></div>');
-	//$("#divDisplay").load(page+".php");
-	$("#divDisplay").delay(1000).queue(function(n){
-		$(this).load(page+".php");
-		n();
-	})
+	$("#divDisplay").html('<div class="progress progress-striped active"><div class="progress-bar"  role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 45%; margin-top: 50px;"></div></div>');
+	$("#divDisplay").load(page+".php");
 }
