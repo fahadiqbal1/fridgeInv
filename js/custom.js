7bb5be0fd34492f804a4475380b2907d967e6ea5
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 20:10:09 -0600

    js test

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,10 @@ function loadDiv(page) {
 	var num = 0
 	setTimeout(function(){
 		num + 10;
-		$("#divDisplay").html('<div class="progress progress-striped active"><div class="progress-bar"  role="progressbar" aria-valuenow="'+num+'" aria-valuemin="0" aria-valuemax="100" style="width: 45%"></div></div>');
 		console.log(num);
 	}, 100);
+
+	$("#divDisplay").html('<div class="progress progress-striped active"><div class="progress-bar"  role="progressbar" aria-valuenow="'+num+'" aria-valuemin="0" aria-valuemax="100" style="width: 45%"></div></div>');
 
 	$("#divDisplay").delay(1000).queue(function(n){
 		$(this).load(page+".php");
