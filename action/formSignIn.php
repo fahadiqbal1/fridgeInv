a571fff885e6d556ea60cde927ca5c497468507f
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 23:23:26 -0600

    form processing

--- action/formSignIn.php
+++ action/formSignIn.php
@@ -1,2 +1,17 @@
 <?php
-print_r($_POST);
+$output = '';
+
+	if(isset($_POST['inputUsername'])){
+		$usr = $_POST['inputUsername'];
+	}
+	if(isset($_POST['inputPassword'])){
+		$pwd = $_POST['inputPassword'];
+	}
+
+	$pwdLen = strlen($pwd);
+	if($pwdLen < 8){
+		$output = "Password needs to be more than 8 characters";
+	}
+	else{
+		$output = "signing in...";
+	}
