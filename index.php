fc165751dd5e93c0f399a034615bcff6881b25fa
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 20:41:57 -0600

    add navbar

--- index.php
+++ index.php
@@ -16,6 +16,29 @@
 		<![endif]-->
 	</head>
 	<body>
+		<nav class="navbar navbar-default" role="navigation">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Fridge Inventory</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a onclick="loadDiv('home')"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+					<li><a href="#">Link</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a onclick="loadDiv('login')">Sign Up / Log In</a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</nav>
 		<div class="container">
 			<button class="btn btn-primary" id="btnHome" onclick="loadDiv('home')"><span class="glyphicon glyphicon-home"></span> Home</button>
 			<button class="btn btn-success" id="btnLogin" onclick="loadDiv('login')">Sign Up / Log In</button>
