b83851881fbe63043cbf8b7aa344289826203620
Fahad Iqbal <iqbal.fahad90@gmail.com>
2013-09-07 20:15:41 -0600

    js test

--- js/custom.js
+++ js/custom.js
@@ -5,14 +5,9 @@ $(function() {
 });
 
 function loadDiv(page) {
+	$("#divDisplay").html('<div class="progress progress-striped active"><div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100"></div></div>');
 	//$("#divDisplay").load(page+".php");
-	var num = 0
-	setTimeout(function(){
-		num + 10;
-		console.log(num);
-	}, 100);
 
-	$("#divDisplay").html('<div class="progress progress-striped active"><div class="progress-bar"  role="progressbar" aria-valuenow="'+num+'" aria-valuemin="0" aria-valuemax="100" style="width: '+num+'%"></div></div>');
 
 	$("#divDisplay").delay(1000).queue(function(n){
 		$(this).load(page+".php");
